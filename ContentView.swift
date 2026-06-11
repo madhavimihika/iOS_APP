5e048da95b77901f0c60e0ad40724bc99449d2ff
mihikamadhavi <mihikamadhavi123@gmail.com>
2026-06-11 15:45:29 +0530

    add UI Code

---
+++ ContentView.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+import Combine
+
+struct ContentView: View {
+    @State private var score = 0// variable score(state as said)
+    @State private var timeRemaining = 10 //variable timer
+    @State private var gameActive = false //boolean
+
+    let timer = Timer.publish(every: 1, on: .main, in: .common)
+        .autoconnect()
+
+    var body: some View {
+        VStack(spacing: 40) {//layout
+
+            Text("Time: \(timeRemaining)s")//timer
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            
+
+            Button(action: {
+                if gameActive {
+                    score += 1// score increase
+                }
+            }) {
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 500, height: 300)
+                    .overlay(
+                        Text("Hit Me")
+                            .font(.title)
+                            .foregroundColor(.yellow)
+                            .bold()
+                    )
+            }
+            Text("Score: \(score)")
+                .font(.title)
+                .bold()
+            
+            Button(gameActive ? "Playing" : "Start Game") {
+                startGame()
+                    
+            }
+            .disabled(gameActive)
+        }
+        .padding()
+        .onReceive(timer) { _ in
+            guard gameActive else { return }
+
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            } else {
+                gameActive = false
+            }
+        }
+    }
+
+    func startGame() {
+        score = 0
+        timeRemaining = 10
+        gameActive = true
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_APPApp.swift
+//  iOS_APP
+//
+//  Created by cobsccomp251p-24 on 2026-06-11.
+//
+
+import SwiftUI
+
+@main
+struct iOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
